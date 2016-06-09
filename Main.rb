d1b6c3c4112d49e486765c9818a8c8e5a66177ac
xXDEF4LTXx <cmurg00@gmail.com>
2016-06-09 18:32:09 +0100

    Update Main.rb

--- Main.rb
+++ Main.rb
@@ -1,4 +1,6 @@
 begin
+  require 'rubygems'
+  require 'mechanize'
 	require 'resolv'
 	require 'colorize'
 	require 'net/http'
@@ -37,6 +39,7 @@ begin
   		puts"1> Webpage cloning"
   		puts"2> Email scrape from URL"
   		puts"3> URL Info grabber"
+      puts"4> SQLI Scanner: \n this module uses your entered google dork\n to search google for basic vulnerable\n websites."
   		choice1 = gets.chomp
   		if choice1 == "1"
 			puts
@@ -54,9 +57,9 @@ begin
     			File.open("#{target}.html", 'w') { |file| file.write(source) }
     			puts
     			puts "Webpage cloned and saved in active directory."
-  
+
   		elsif choice1.to_s == "2"
-			puts 
+			puts
 			puts "Don't use 'http://'. Use 'www.'."
 			print"Enter site you want to scrape emails: "
 			target=gets.chomp
@@ -77,10 +80,10 @@ begin
 			puts"#{address}".green
 			CSV.open("emails.csv", "a") do |csv|
 				csv << ["#{address}"]
-		    	end 
+		    	end
 
 
-			end   
+			end
 			end
 			end
 	 	elsif choice1.to_s == "3"
@@ -101,6 +104,62 @@ begin
 			puts "First IP listed: #{ip}"
 			puts "All listed IPs: #{ips}"
 			puts "All listed hostnames: #{hname}"
+    elsif choice1.to_s == "4"
+      begin
+            agent = Mechanize.new
+            #agent.user_agent_alias = 'Mechanize'
+            #agent.set_proxy '78.186.178.153', 808
+            agent.history.clear
+            page = agent.get('http://www.google.com/')
+            google_form = page.form('f')
+            puts "Enter SQLI Dork: "
+            dork = gets.chomp
+            google_form.q = dork.to_s
+
+            page = agent.submit(google_form, google_form.buttons.first)
+
+            page.links.each do |link|
+              if link.href.to_s =~/url.q/
+                str=link.href.to_s
+                strList=str.split(%r{=|&})
+                url=strList[1]
+                puts url
+                checkur = url.gsub("%3F", "?")
+                checkurr = checkur.gsub("%3D", "=")
+                checkurl = checkur
+                puts checkurl +' a'
+                begin
+                  source = open(checkurl).read
+                  aa=source.scan('You have an error in your SQL syntax')
+                  a= aa.to_s
+                  if a == '["You have an error in your SQL syntax"]'
+                      puts 'Vulnerability possible'.green
+                      url2 = "\n #{url.to_s} \n"
+                      towrite = "\n #{url2} \n"
+                      file = 'SQL.txt'
+                      File.open(file, 'a') { |file| file.write(towrite) }
+                  else
+                      puts 'No basic vulnerability'.red
+                  end
+                rescue OpenURI::HTTPError => err
+                      puts "Error in link or network".red
+                      elog = "Error2.log"
+                      write = "\n #{err} \n"
+                      File.open(elog, 'a') { |file| file.write(write) }
+                end
+
+              end
+            end
+            puts "All vulnerable links have been entered into SQL.txt."
+      rescue Exception => er
+        time1 = Time.new
+        logtime = "Error Time : " + time1.inspect
+        File.open('SQLerror.log', 'a') { |file| file.write("\n"+logtime.to_s+"\n") }
+        err = er.inspect
+        File.open('SQLerror.log', 'a') { |file| file.write("\n"+err.to_s+"\n") }
+        err2 = er.backtrace
+        File.open('SQLerror.log', 'a') { |file| file.write("\n"+err2.to_s+"\n") }
+      end
 		else
 			puts "Unknown response."
 		end
@@ -113,7 +172,7 @@ begin
 	else
   	puts "Unknown response."
 	end
-	
+
 rescue Exception => e
   time1 = Time.new
   logtime = "Error Time : " + time1.inspect
