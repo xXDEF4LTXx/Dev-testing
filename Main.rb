a515b4c72589cb6c8239dec11c3028b88eda1d42
xXDEF4LTXx <cmurg00@gmail.com>
2016-06-12 17:13:02 +0000

    Update Main.rb

--- Main.rb
+++ Main.rb
@@ -1,13 +1,14 @@
 begin
   require 'rubygems'
+  require 'fileutils'
   require 'mechanize'
 	require 'resolv'
 	require 'colorize'
 	require 'net/http'
-	require'spidr'
+	require 'spidr'
 	require 'open-uri'
 	require 'uri'
-	require'csv'
+	require 'csv'
 
 	puts
 	puts"FFFFFFFFFFFFFFFFFFFFFF     AAA    WWWWWWWW                           WWWWWWWW".blue
@@ -40,12 +41,17 @@ begin
   		puts"2> Email scrape from URL\n".blue
   		puts"3> URL Info grabber\n".blue
       puts"4> SQLI Scanner: \n this module uses your entered google dork\n to search google for basic vulnerable\n websites.\n".blue
-  		choice1 = gets.chomp
+      puts"5> Check online databases to see if your \ninformation has been breached.\nA breach means that your entered\nusername or email has appeared in\nsome leaked details.\nA breach can be sensitive or non.\nThis means plain-text passwords etc.\n".blue
+      choice1 = gets.chomp
   		if choice1 == "1"
 			puts
     			puts "Use 'www.' and don't use 'http://'.".red
     			puts "Enter target URL: ".blue
-    			target = gets.chomp
+    			targ = gets.chomp.to_s
+          target = URI"http://#{targ}"
+          puts "Enter Folder Name To Create And Save Files To.\nThis folder will be nested in the active directory.\nFolder name: \n"
+          folder = gets.chomp
+          Dir.mkdir(folder) unless File.exists?(folder)
     			puts
     			puts "Initializing...".red
     			sleep 1
@@ -53,16 +59,29 @@ begin
     			sleep 1
     			puts "Fire!".green
     			sleep 1
-    			source = open('http://'+target).read
-    			File.open("#{target}.html", 'w') { |file| file.write(source) }
-    			puts
-    			puts "Webpage cloned and saved in active directory.".green
+          puts 'a'
+          Spidr.site(target) do |spider|
+            puts 'b'
+    			spider.every_page do |crawled|
+            puts 'c'
+          puts crawled.url
+          puts 'd'
+    			source = open(crawled.url).read
+          Dir.chdir(folder) do
+            File.open("#{crawled.url}.html", 'w') { |file| file.write(source) }
+          end
+          puts
+    			puts "Website cloned and saved in active directory.".green
+          end
+          end
 
   		elsif choice1.to_s == "2"
 			puts
 			puts "Don't use 'http://'. Use 'www.'.".red
 			print"Enter site you want to scrape emails: ".blue
 			target=gets.chomp
+      puts "Name of the file to save results to:\n"
+      filenames=gets.chomp
 			puts
    		puts "Initializing...".red
     	sleep 1
@@ -78,7 +97,8 @@ begin
 			puts "#{crawled_page.url}".blue
 			crawled_page.body.scan(/[\w\d]+[\w\d.-]@[\w\d.-]+\.\w{2,6}/).each do |address|
 			puts"#{address}".green
-			CSV.open("emails.csv", "a") do |csv|
+
+			CSV.open("#{filenames}.csv", "a") do |csv|
 				csv << ["#{address}"]
 		    	end
 
@@ -115,6 +135,7 @@ begin
             sleep 1
             puts
             agent = Mechanize.new
+            agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
             counting = 0
             #agent.user_agent_alias = 'Mechanize'
             #agent.set_proxy '78.186.178.153', 808
@@ -129,8 +150,10 @@ begin
             google_form.q = dork.to_s
 
             page = agent.submit(google_form, google_form.buttons.first)
-
             page.links.each do |link|
+            page2 = agent.get("#{page.uri}&start=10")
+            page2.links.each do |link|
+
               if link.href.to_s =~/url.q/
                 str=link.href.to_s
                 strList=str.split(%r{=|&})
@@ -139,8 +162,13 @@ begin
                 checkur = url.gsub("%3F", "?")
                 checkurr = checkur.gsub("%3D", "=")
                 checkurl = checkur
+                #puts "#{page.uri}".green
                 begin
-                  source = open(checkurl).read
+                  #ax = checkurl.scan('https://facebook')
+                  #if ax.to_s == '["https://facebook"]'
+                  #  checkurl = 'http://lol.com/gjouyhju.php?id=3'#throw 404 to prevent SSL error
+                  #end
+                  source = open(checkurl, {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}).read
                   aa=source.scan('You have an error in your SQL syntax')
                   a= aa.to_s
                   if a == '["You have an error in your SQL syntax"]'
@@ -162,6 +190,7 @@ begin
 
               end
             end
+            end
             puts "All #{counting} vulnerable links have been entered into SQL.txt."
       rescue Exception => er
         time1 = Time.new
@@ -172,7 +201,39 @@ begin
         err2 = er.backtrace
         File.open('SQLerror.log', 'a') { |file| file.write("\n"+err2.to_s+"\n") }
       end
-		else
+    elsif choice1.to_s == "5"
+      puts "\nEnter your username or email address to be checked\nthrough the databases:\n".blue
+      checkdetail=gets.chomp.to_s
+      puts "Enter file name to save results to:\n"
+      filename=gets.chomp
+      url=URI.parse("https://haveibeenpwned.com/api/v2/breachedaccount/#{checkdetail}")
+      output = open(url, {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE})
+      result = JSON.parse output.readlines.join("")
+      puts "Breaches".green
+      puts "--------\n".red
+      coun = result.count
+      count = coun +=1
+      breachnum=0
+      x=0
+      puts "Amount of breaches: #{result.count}".red
+      File.open("#{filename}.txt", 'a') { |file| file.write("\nAmount of breaches: #{result.count}\n") }
+      while breachnum < count
+        breachnum+=1
+        puts "\nTitle of data breach number #{breachnum}: #{result[x]['Title']}".green
+        File.open("#{filename}.txt", 'a') { |file| file.write("\nTitle of data breach number #{breachnum}: #{result[x]['Title']}\n") }
+        puts "\nName of data breach number #{breachnum}: #{result[x]['Name']}".red
+        File.open("#{filename}.txt", 'a') { |file| file.write("\nName of data breach number #{breachnum}: #{result[x]['Name']}\n") }
+        puts "\nDomain of data breach number #{breachnum}: #{result[x]['Domain']}".blue
+        File.open("#{filename}.txt", 'a') { |file| file.write("\nDomain of data breach number #{breachnum}: #{result[x]['Domain']}\n") }
+        puts "\nDate breach number #{breachnum} added to breach database: #{result[x]['AddedDate']}\n".green
+        File.open("#{filename}.txt", 'a') { |file| file.write("\nDate breach number #{breachnum} added to breach database: #{result[x]['AddedDate']}") }
+        puts "\nData types of breach number #{breachnum}: #{result[x]['DataClasses']}".red
+        File.open("#{filename}.txt", 'a') { |file| file.write("\nData types of breach number #{breachnum}: #{result[x]['DataClasses']}\n") }
+        puts "\nData sensitive in breach number #{breachnum}? #{result[x]['IsSensitive']}".blue
+        File.open("#{filename}.txt", 'a') { |file| file.write("\nData sensitive in breach number #{breachnum}? #{result[x]['IsSensitive']}\n") }
+        x+=1
+      end
+    else
 			puts "Unknown response.".red
 		end
 	elsif choice.to_s == "2"
